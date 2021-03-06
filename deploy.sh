c4e23c3d14614464ad06f4000e351e419fca310b
mchadha716 <41342617+mchadha716@users.noreply.github.com>
2021-03-05 23:19:47 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,12 @@
-#!/bin/bash
-
 export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
 export MIX_ENV=prod
-export PORT=4790
-export NODEBIN=`pwd`/assets/node_modules/.bin
+export PORT=4799
+export NODEBIN='pwd'/assets/node_modules/.bin
 export PATH="$PATH:$NODEBIN"
-
-echo "Building..."
 
 mix deps.get
 mix compile
-(cd assets && npm install)
-(cd assets && webpack --mode production)
+(cd assets && npm install && npm run deploy)
 mix phx.digest
 
-echo "Generating release..."
 mix release
-
-#echo "Stopping old copy of app, if any..."
-#_build/prod/rel/practice/bin/practice stop || true
-
-echo "Starting app..."
-
-PROD=t ./start.sh
