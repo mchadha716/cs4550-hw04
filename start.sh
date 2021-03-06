39e7c13fc8b7bb04d8b5776cafa49157278eb280
mchadha716 <41342617+mchadha716@users.noreply.github.com>
2021-03-05 23:18:57 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,27 +1,9 @@
 #!/bin/bash
 
-#if [[ "x$PROD" == "x" ]]; then 
-#	echo "This script is for starting in production."
-#	echo "Use"
-#	echo "   mix phx.server"
-#	exit
-#fi
-
-# Enable this script by removing the above.
-
 export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
 export MIX_ENV=prod
-export PORT=4790
-
-echo "Stopping old copy of app, if any..."
-
-_build/prod/rel/practice/bin/practice stop || true
-
-echo "Starting app..."
+export PORT=4799
+export NODEBIN='pwd'/assets/node_modules/.bin
+export PATH="$PATH:$NODEBIN"
 
 _build/prod/rel/practice/bin/practice start
-
-# Add a systemd service file
-#       to start your app on system boot.
-
-sudo mv hw04.service /etc/systemd/system
